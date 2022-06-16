35559a5f8bcbb0532ea6382cd8f3a46b45d53c62
Zurihaqi <zurihaqi@gmail.com>
2022-06-16 16:23:40 +0700

    Menambahkan seeder cities dan memperbaiki urutan migration

---
+++ server/db/seeders/20220616075511-cities-seeder.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const citiesMasterdata = require('../masterdata/cities.json');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const cityData = citiesMasterdata.map((eachCityData) => {
+      return{
+        "name": eachCityData.name,
+        "created_at": new Date().toLocaleString('en-GB', {timezone: 'UTC+7'}),
+        "updated_at": new Date().toLocaleString('en-GB', {timezone: 'UTC+7'})
+      };
+    });
+    const sortByCityName = cityData.sort((a, b) => a.name > b.name ? 1 : -1);
+    await queryInterface.bulkInsert('cities', sortByCityName);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('cities', null, { truncate: { cascade: true }, restartIdentity: true });
+  }
+};
