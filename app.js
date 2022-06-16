c48a599012b08561ae7cb3cc25b1c266ac8ab9ef
Wahyu <kyu661ll3@gmail.com>
2022-06-16 10:10:56 -0700

    menambahkan controller dan router untuk autentikasi

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-require('dotenv').config();
 const express = require('express');
+const path = require("path");
+const logger = require("morgan");
+
+// call all module route here
+const authRouter = require("./app/api/auth/router");
+
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(authRouter);
+app.use("/", (req, res) => {
+    res.json({
+        message: "Selamat Datang di API SecondHand"
+    })
+})
+
 
 module.exports = app;
