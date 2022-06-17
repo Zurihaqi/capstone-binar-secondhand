cd713ace75f80f0d6f3e6c3bc8054b50185b7882
Zurihaqi <zurihaqi@gmail.com>
2022-06-17 12:18:05 +0700

    menambahkan product_image controller, validation, route

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const router = require("./server/routes/app.routes");
+const logger = require('morgan');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger('combined'));
+
+app.use('/api', router);
 
 module.exports = app;
---
+++ server/validations/productImage.validation.js
@@ -0,0 +1,41 @@
+const { body, param } = require('express-validator');
+
+const getProductImageById = () => {
+    param('id')
+    .notEmpty()
+    .withMessage('id cannot be empty');
+};
+
+const createProductImage = () => {
+    body('image_url')
+    .notEmpty
+    .withMessage('product image cannot be empty');
+    body('products_id')
+    .notEmpty
+    .withMessage('products_id cannot be empty');
+};
+
+const updateProductImage = () => {
+    body('image_url')
+    .notEmpty
+    .withMessage('product image cannot be empty');
+    body('products_id')
+    .notEmpty
+    .withMessage('products_id cannot be empty');
+    param('id')
+    .notEmpty()
+    .withMessage('id cannot be empty');
+};
+
+const deleteProductImage = () => {
+    param('id')
+    .notEmpty()
+    .withMessage('id cannot be empty');
+};
+
+module.exports = {
+    getProductImageById,
+    createProductImage,
+    updateProductImage,
+    deleteProductImage
+};
