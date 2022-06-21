e61eea81ab9a309a4303aa1c08b8736bc37f085e
NineOne <ihim512@gmail.com>
2022-06-21 09:05:12 +0700

    initial feature tenders

---
+++ server/controllers/tenders.controller.js
@@ -0,0 +1,118 @@
+const { Tender, Product, User } = require("../db/models");
+const errors = require("../misc/errors");
+
+const options = {
+  attributes: {
+    exclude: ["updated_at"],
+  },
+  include: [
+    {
+      model: Product,
+      attributes: {
+        exclude: ["created_at", "updated_at"],
+      },
+    },
+    {
+      model: User,
+      attributes: {
+        exclude: ["password", "created_at", "updated_at"],
+      },
+    },
+  ],
+};
+
+module.exports = {
+  addTender: async (req, res) => {
+    try {
+      const { offer_status, price, users_id, products_id } = req.body;
+
+      //? Cek ketersediaan
+      const userExist = await User.findByPk(users_id);
+      const productExist = await Product.findByPk(products_id);
+      const tenderExist = await Tender.findOne({
+        where: {
+          users_id: users_id,
+          products_id: products_id,
+        },
+      });
+
+      //! Cek Error
+      if (!userExist) throw errors.NOT_FOUND("User", users_id);
+      if (!productExist) throw errors.NOT_FOUND("Product", products_id);
+      if (tenderExist) throw errors.AVAILABLE_DATA("Tender", tenderExist.id);
+
+      const tender = await Tender.create({
+        offer_status: offer_status,
+        price: price,
+        users_id: users_id,
+        products_id: products_id,
+      });
+      return res.status(200).json({
+        status: "Tender created successfully",
+        data: tender,
+      });
+    } catch (error) {
+      if (error.code) {
+        return res.status(error.code).json({
+          status: error.status,
+          message: error.message,
+        });
+      }
+      return res.status(500).json({
+        status: "Internal server error",
+        message: error.message,
+      });
+    }
+  },
+  getAllTenders: async (req, res) => {
+    try {
+      const tenders = await Tender.findAll(options);
+      if (tenders) {
+        return res.status(200).json({
+          status: "Success",
+          data: tenders,
+        });
+      }
+      throw {
+        code: 404,
+        status: "Not Found",
+        message: "You not have Tender",
+      };
+    } catch (error) {
+      if (error.code) {
+        return res.status(error.code).json({
+          status: error.status,
+          message: error.message,
+        });
+      }
+      return res.status(500).json({
+        status: "Internal server error",
+        message: error.message,
+      });
+    }
+  },
+  getTenderById: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const tender = await Tender.findByPk(id, options);
+      if (tender) {
+        return res.status(200).json({
+          status: "Success",
+          data: tender,
+        });
+      }
+      throw errors.NOT_FOUND("Tenders", id);
+    } catch (error) {
+      if (error.code) {
+        return res.status(error.code).json({
+          status: error.status,
+          message: error.message,
+        });
+      }
+      return res.status(500).json({
+        status: "Internal server error",
+        message: error.message,
+      });
+    }
+  },
+};
