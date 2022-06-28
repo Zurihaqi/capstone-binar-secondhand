ab2cc5972990d738015924078a0050370381b060
Zurihaqi <zurihaqi@gmail.com>
2022-06-28 22:56:12 +0700

    auth validation fix

--- server/validations/auth.validation.js
+++ server/validations/auth.validation.js
@@ -8,9 +8,9 @@ module.exports = {
   register: () => [
     body("name")
       .isAlpha("en-US", { ignore: " " })
+      .withMessage("Name cannot contain number or symbols")
       .isLength({ min: 3 })
-      .withMessage("Name must be at least 3 characters long")
-      .withMessage("Enter a valid name"),
+      .withMessage("Name must be at least 3 characters long"),
     body("email").isEmail().normalizeEmail().withMessage("Enter a valid email"),
     body("password").notEmpty().withMessage("Enter a password"),
   ],
