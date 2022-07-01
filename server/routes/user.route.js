0c3cf8d2bb447994be82ce700972e56509d8d1df
NineOne <ihim512@gmail.com>
2022-07-01 21:10:52 +0700

    update bug users and auth

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,14 +1,24 @@
 const router = require("express").Router();
 const validation = require("../validations/user.validation");
 const validate = require("../middlewares/validation");
+const multer = require("../middlewares/multer");
+const cloudinaryUpload = require("../middlewares/cloudinary");
 const {
   getUserById,
   updateUser,
   deleteUserById,
 } = require("../controllers/users.controller");
 
-router.get("/:id", validation.getUserById, validate, getUserById);
-router.patch("/:id", validation.updateUser, validate, updateUser);
-router.delete("/:id", validation.deleteUser, validate, deleteUserById);
+router.get("/", validation.getUserById(), validate, getUserById);
+router.patch(
+  "/",
+  multer.imageUpload.single("photo_profile"),
+  multer.errorHandler,
+  validation.updateUser(),
+  validate,
+  cloudinaryUpload,
+  updateUser
+);
+router.delete("/", validation.deleteUser(), validate, deleteUserById);
 
 module.exports = router;
