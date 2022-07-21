b4f19bd1d50957c4c687790ca6e99b3e700204da
NineOne <ihim512@gmail.com>
2022-07-21 13:35:40 +0700

    revision app js

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ app.use(logger("combined"));
 app.use("/api", router);
 app.use("/", (req, res) => {
   // opens the url in the default browser
-  open("https://documenter.getpostman.com/view/13273250/UzJFweL2");
+  // open("https://documenter.getpostman.com/view/13273250/UzJFweL2");
   res.json({
-    message: "Selamat Datang di API SecondHand",
+    message:
+      "Selamat Datang di API SecondHand, Silhakan cek dokumentasi disini: https://documenter.getpostman.com/view/13273250/UzJFweL2",
   });
 });
 
